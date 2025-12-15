509eccc452a9e5b90c375553c3c10a1c330aa54e
Sam Nolan <51299959+TheSam912@users.noreply.github.com>
2025-12-15 11:54:20 +0330

    nothing

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -118,7 +118,7 @@
 
             {{-- MOBILE hamburger --}}
             <div class="md:hidden">
-                <button @click="open = !open" class="p-2 rounded-lg hover:bg-gray-100">
+                <button @click="open = !open" class="p-2 rounded-lg">
                     <svg class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                             d="M4 6h16M4 12h16M4 18h16" />
